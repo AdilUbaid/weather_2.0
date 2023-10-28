a1c06b391f2c8f1e7388313f04bfab839b15f252
Adil Ubaid <adilubaid1999i@gmail.com>
2023-10-28 19:54:28 +0530

    color

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color lightBlue = const Color(0xAECDFF);
+Color purple = const Color(0x806EF8);
+Color blue = const Color(0x5896FD);
+Color black = const Color(0x1A1D96);
